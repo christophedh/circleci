3465d6ea2b67e19801be1ebb7aa5561a1be6697a
christophe <christophe@localhost.localdomain>
2020-09-14 15:36:47 -0400

    fix test

--- main_test.go
+++ main_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestHello(t *testing.T) {
 	expect := hello("hello")
-	if expect != "hello you1" {
-		t.Errorf("hello(hello); want hello you got %s", expect)
+	if expect != "hello you" {
+		t.Errorf("hello(hello); want \"hello you\" got \"%s\"", expect)
 	}
 }
